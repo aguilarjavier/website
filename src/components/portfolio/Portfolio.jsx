96694ac612201761b0cc6004c11f5f0240922211
Javier <javierene1064@gmail.com>
2022-08-02 22:09:08 -0400

    started working on navabar min36

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,6 @@ import "./portfolio.scss"
 
 export default function portfolio() {
   return (
-    <div className='portfolio'>portfolio</div>
+    <div className='portfolio' id="porfolio">portfolio</div>
   )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
   return (
-    <div className='topbar'>Topbar</div>
+    <div className='topbar'>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className='logo'>genius.</a>
+          <div className="itemContainer">
+            <Person/>
+            <span>linkedin</span>
+          </div>
+          <div className="itemContainer">
+          <Mail/>
+            <span>javieraguilar@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          this is right
+        </div>
+      </div>
+    </div>
   )
 }
